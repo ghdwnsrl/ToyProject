af51e93a312b4e90ffa329b51b6209d2163ee22b
hong jun ki <ghdwnsrl1991@gmail.com>
2024-07-15 18:14:59 +0900

    [test] Comment 삭제 시 발생하는 예외 코드 테스트

--- src/test/java/study/login/comment/service/CommentServiceTest.java
+++ src/test/java/study/login/comment/service/CommentServiceTest.java
@@ -1,4 +1,4 @@
-package study.login.comment;
+package study.login.comment.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +11,7 @@ import study.login.comment.controller.port.CommentService;
 import study.login.comment.domain.Comment;
 import study.login.comment.domain.CommentDto;
 import study.login.comment.domain.CommentListDto;
-import study.login.comment.service.CommentServiceImpl;
+import study.login.common.exception.PermissionDeniedException;
 import study.login.member.controller.port.MemberService;
 import study.login.member.domain.Member;
 import study.login.member.service.MemberServiceImpl;
@@ -21,6 +21,8 @@ import study.login.mock.FakeMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class CommentServiceTest {
 
@@ -51,6 +53,38 @@ public class CommentServiceTest {
     }
 
     @Test
+    void 댓글_작성자가_아닌_사람이_삭제하면_예외가_발생한다() {
+        // given
+        Member member = Member.builder()
+                .userId("hong")
+                .password("pw111")
+                .nickname("hongs")
+                .build();
+
+        ArticleWriteForm articleWriteForm = ArticleWriteForm.builder()
+                .title("title")
+                .contents("contents")
+                .build();
+
+        Article article = articleService.write(articleWriteForm, member);
+
+        CommentDto commentDto = CommentDto.builder()
+                .articleId(article.getId())
+                .nickname(member.getNickname())
+                .contents("댓글 1")
+                .build();
+
+        Comment comment = commentService.write(commentDto);
+
+        // when
+        // then
+        assertThatThrownBy(()->commentService.deleteById(comment.getId(), "kim"))
+                .isInstanceOf(PermissionDeniedException.class);
+
+    }
+
+
+    @Test
     void CommentDto로_댓글을_달_수_있다() {
         // given
         Member member = Member.builder()
@@ -76,13 +110,13 @@ public class CommentServiceTest {
         Comment comment = commentService.write(commentDto);
 
         // then
-        Assertions.assertThat(comment.getId()).isNotNull();
-        Assertions.assertThat(comment.getContents()).isEqualTo("댓글 1");
-        Assertions.assertThat(comment.getNickname()).isEqualTo(member.getNickname());
-        Assertions.assertThat(comment.getArticle().getId()).isEqualTo(article.getId());
-        Assertions.assertThat(comment.getArticle().getTitle()).isEqualTo(article.getTitle());
-        Assertions.assertThat(comment.getArticle().getMember()).isNotNull();
-        Assertions.assertThat(comment.getArticle().getViews()).isEqualTo(article.getViews());
+        assertThat(comment.getId()).isNotNull();
+        assertThat(comment.getContents()).isEqualTo("댓글 1");
+        assertThat(comment.getNickname()).isEqualTo(member.getNickname());
+        assertThat(comment.getArticle().getId()).isEqualTo(article.getId());
+        assertThat(comment.getArticle().getTitle()).isEqualTo(article.getTitle());
+        assertThat(comment.getArticle().getMember()).isNotNull();
+        assertThat(comment.getArticle().getViews()).isEqualTo(article.getViews());
     }
 
     @Test
@@ -129,10 +163,10 @@ public class CommentServiceTest {
         List<CommentListDto> result = commentService.findList(article.getId());
 
         // then
-        Assertions.assertThat(result.size()).isEqualTo(3);
-        Assertions.assertThat(result.get(0).getContents()).isEqualTo("댓글 1");
-        Assertions.assertThat(result.get(1).getContents()).isEqualTo("댓글 2");
-        Assertions.assertThat(result.get(2).getContents()).isEqualTo("댓글 3");
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0).getContents()).isEqualTo("댓글 1");
+        assertThat(result.get(1).getContents()).isEqualTo("댓글 2");
+        assertThat(result.get(2).getContents()).isEqualTo("댓글 3");
 
     }
 
@@ -162,11 +196,11 @@ public class CommentServiceTest {
 
         // when
         Optional<Comment> before = commentRepository.findById(comment.getId());
-        commentService.deleteById(comment.getId());
+        commentService.deleteById(comment.getId(), "hongs");
         Optional<Comment> after = commentRepository.findById(comment.getId());
         // then
-        Assertions.assertThat(before).isPresent();
-        Assertions.assertThat(after).isNotPresent();
+        assertThat(before).isPresent();
+        assertThat(after).isNotPresent();
     }
 
     @Test
@@ -213,7 +247,7 @@ public class CommentServiceTest {
         List<CommentListDto> result = commentService.findList(article.getId());
 
         // then
-        Assertions.assertThat(result).isEmpty();
+        assertThat(result).isEmpty();
     }
 
 }
